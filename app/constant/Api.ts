e7e82ff415196b27c9cfeb9fb8a0b32c63875cd5
febintjames <126459080+febintjames@users.noreply.github.com>
2025-11-19 19:20:39 +0530

    updated temp api draft

---
+++ app/constant/Api.ts
@@ -0,0 +1,5 @@
+export class ApiConst {
+  static readonly BASE_URL = "https://kioskapi.qmark.com/api";
+  static readonly GET_PRESIGN_URL = "/presign-upload";
+  static readonly UPLOAD_USER_DATA = "/upload-user-data";
+}
